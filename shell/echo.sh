56f84506c46ae55c085e9687d61b1e31fb4ec25b
xionglong58 <xionglong58@email.swu.edu.cn>
2017-12-12 00:18:58 -0800

    -

--- shell/echo.sh
+++ shell/echo.sh
@@ -1,8 +1,30 @@
-#!/bin/sh
-word="hello world"
 name="knight"
-number=1
-echo "${word},${name} said ${number}"
-#readonly name
-unset name
-echo ${name}
+word="hello world"
+echo "${name} said ${word}"
+
+readonly name
+name="night"
+unset word
+echo ${word}
+
+echo '${name}'
+echo ${#name}
+
+string="the more you know, the more you don't know"
+len=${#string}
+echo ${string:0:len}
+
+echo `expr index "${string}" "e"`
+
+set=(0 1 2 3 'xionglong')
+len1=${#set[@]}
+set1=${set[4]}
+
+setall=${set[@]}
+echo "${len1},${set1},${setall}"
+
+echo "${string:0-4:4}"
+echo "${string#*you}"
+echo "${string##*you}"
+echo "${string%you*}"
+echo "${string%%you*}"
